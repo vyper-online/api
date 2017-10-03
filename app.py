ef1aefce4fbe819e865d3fe2a8053a89e161f052
holgerd77 <Holger.Drewes@gmail.com>
2017-10-03 12:15:20 +0200

    Added actual compilation to API compile function

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-import json, requests
+import json, logging, requests, sys
 from flask import Flask, jsonify, make_response, request
 from flask_cors import CORS
+import viper
+from viper import compiler, optimizer
+from viper.parser import parse_to_lll
 
+logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -9,12 +13,22 @@ cors = CORS(app)
 COMPILE_ENDPOINT = '/compile/'
 
 
-@app.route(COMPILE_ENDPOINT, methods=['GET',])
+@app.route(COMPILE_ENDPOINT, methods=['GET', 'POST'])
 def compile():
-    q = request.args.get('q', '')
-    print(q)
+    source = request.form.get('source', '')
+    abi = compiler.mk_full_signature(code)
+    json_abi = json.dumps(compiler.mk_full_signature(code))
+    bytecode = '0x' + compiler.compile(code).hex()
+    ir = optimizer.optimize(parse_to_lll(code))
     
-    r_dict = { 'result': { 'msg': 'All looks fine', 'code': 200 } }
+    r_dict = { 'result': {
+        'msg': 'Compilation successful',
+        'abi': abi,
+        'json': json_abi,
+        'bytecode': bytecode,
+        'ir': ir,
+        'code': 200, }
+    }
     return make_response(jsonify(r_dict), 200)
 
 
